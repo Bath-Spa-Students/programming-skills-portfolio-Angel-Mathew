1d16941f899a0cdd447ad3dcb2fb8e969a423829
Angel-Mathew <144330644+Angel-Mathew@users.noreply.github.com>
2023-10-09 10:40:01 +0400

    turtle graphic

--- Chapter 1- Getting Started with Python/Exercises/practice3.py
+++ Chapter 1- Getting Started with Python/Exercises/practice3.py
@@ -1,1 +1,8 @@
 #Question 3 in practice worksheet:You need to make a program for a leader board.The program needs to output the numbers 1 to 9 each on a separate line followed by a dot
+Leader1=input("Enter leader's name:")
+Leader2=input("Enter leader2's name:")
+Leader3=input("Enter leader3's name:")
+Leader4=input("Enter leader4's name:")
+Leader5=input("Enter leader5's name:")
+Leader6=input("Enter leader6's name:")
+Leader7=input("Enter leader7's name:")
