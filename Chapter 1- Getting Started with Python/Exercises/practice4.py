16607ca614a655b7fa78a6107370b22ac899205b
Angel-Mathew <144330644+Angel-Mathew@users.noreply.github.com>
2023-10-11 13:40:25 +0400

    practice worksheets

--- Chapter 1- Getting Started with Python/Exercises/practice4.py
+++ Chapter 1- Getting Started with Python/Exercises/practice4.py
@@ -1,1 +1,3 @@
 #Question 4 in practice worksheet:Compute the area of triangle
+area=14*12/2
+print(int(area))
--- Chapter 1- Getting Started with Python/Exercises/practice5.py
+++ Chapter 1- Getting Started with Python/Exercises/practice5.py
@@ -1,1 +1,2 @@
 #Question 5 in practice worksheet:Write four strings in different variables and print the output as one string 
+print("My_" "name_" "is_" "Angel" )
