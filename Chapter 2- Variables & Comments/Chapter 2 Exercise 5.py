be39bbc1c3f771eb3d78d7c0f2d12c566106a41d
Angel-Mathew <144330644+Angel-Mathew@users.noreply.github.com>
2023-10-16 10:07:49 +0400

    Chapter2 Exercise5

--- Chapter 2- Variables & Comments/Chapter 2 Exercise 5.py
+++ Chapter 2- Variables & Comments/Chapter 2 Exercise 5.py
@@ -1,24 +1,38 @@
-#1method:
- #Number_of_USBStick=(50/6)
+#1method: Number_of_USBStick=(50/6)
 #print(Number_of_USBStick)
 #Total=((50/8))
 ##print(Total)
 #Balance=((50/8))
 #print(Balance)
              
-#2nd method:
-#Number_of_USBsticks=(50/6)
+#2nd method:#Number_of_USBsticks=(50/6)
 #print(Number_of_USBsticks)
 #Total=(50-Number_of_USBsticks)
 #print(Total)
 #Balance=(50-Number_of_USBsticks)
 #print(Balance)
+
 #3 method:
-Total=(50/6)
-print(int(Total))
-Number_of_USBStick=int(input("Enter Number_of_USBSticks:"))
-print(Number_of_USBStick)
-Total_amount=(Total-Number_of_USBStick)
-print (int(Total_amount))
-Balance=(Total_amount-Number_of_USBStick)
-print (Balance)
+#Total_Number_of_USBsticks=(50/6)
+#print(int(Total_Number_of_USBsticks))
+#Total_amount_of_the_USBsticks=(6*Total_Number_of_USBsticks)
+#print(int(Total_amount_of_the_USBsticks))
+#Number_of_USBsticks=int(input("Enter Number_of_USBsticks:"))
+#Total_amount_to_pay=(Total_amount_of_the_USBsticks-Number_of_USBsticks)
+#print(int(Total_amount_to_pay))
+#Balance=(Total_amount_to_pay-Number_of_USBsticks)
+#print(int(Balance))
+
+#4th method:
+#Number_of_USBSticks=int(input("Enter Number_of_USBsticks:"))
+#Total_amount_to_pay=(6*Number_of_USBSticks)
+#print(int(Total_amount_to_pay))
+#Balance=(50-Total_amount_to_pay)
+#print(int(Balance))
+
+# 5th method
+Number_of_USBSticks=(50/6)
+print(int(Number_of_USBSticks))
+Total_amount=int(Number_of_USBSticks)
+Balance=(50-Total_amount)
+print(Balance)
