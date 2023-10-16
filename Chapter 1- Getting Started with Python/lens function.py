ac3da68e2abc8f9fa526bf1be0d25d538e9e4049
Angel-Mathew <144330644+Angel-Mathew@users.noreply.github.com>
2023-10-16 10:27:54 +0400

    classworks

---
+++ Chapter 1- Getting Started with Python/lens function.py
@@ -0,0 +1,7 @@
+#numbers=[8,2,0,0,4,25,9,10,54,28,23,19,20,8,7,7,11,12,2,2,2,2,4,4,4,4,4,4,44,]
+#print("number of elements in a list:",len(numbers))
+#Mutability(changeble)
+numbers=[5,6,7,8,3,4]
+numbers[0]=1
+numbers[1]=8
+print(numbers)
