dd41d4a66f63c280fd8c7167bb2a7b78ede22fbe
Angel-Mathew <144330644+Angel-Mathew@users.noreply.github.com>
2023-10-22 18:56:36 +0400

    Chapter 3 Exercises

---
+++ Chapter 3- Data Structures/Exercises/chapter3 exercise 7.py
@@ -0,0 +1,9 @@
+Location=["USA","Oman","Mexico","UAE","Kerala"]
+print(Location)
+print(sorted(Location))
+print(sorted(Location,reverse=True))
+print(reversed(Location))
+print(sorted(Location))
+print(sorted(Location,reverse=True))
+
+
