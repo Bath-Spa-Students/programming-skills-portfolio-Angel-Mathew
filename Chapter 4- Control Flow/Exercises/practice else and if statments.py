4cee7957392bf5b78af4610d689d7a502e76435b
Angel-Mathew <144330644+Angel-Mathew@users.noreply.github.com>
2023-10-23 10:46:51 +0400

    else and if statements

---
+++ Chapter 4- Control Flow/Exercises/practice else and if statments.py
@@ -0,0 +1,14 @@
+#Salary=float(input("enter salary:"))
+#advance=0
+#if Salary>60000:
+   # advance=600
+#else:
+    #advance=0
+#print("your advance:"+str(advance))    
+Temperature=float(input("enter the temperature:"))
+About_weather1=("Nice weather,we are having")
+if Temperature<40:
+    About_weather=("Nice weather,we are having")
+else:
+    About_weather=("A little cold, is not it?")    
+print("your About_weather:"+str(About_weather))    
