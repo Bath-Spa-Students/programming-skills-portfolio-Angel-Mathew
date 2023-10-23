4adfa36a14825cbd6d1ec63b68d7460b44a3079e
Angel-Mathew <144330644+Angel-Mathew@users.noreply.github.com>
2023-10-23 09:44:09 +0400

    update chapter 3 exercises  6 and 7
    This reverts commit 76d0813c1aea43044f743984fe2831f1424bddf3.

--- Chapter 3- Data Structures/Exercises/chapter3 exercise 7.py
+++ Chapter 3- Data Structures/Exercises/chapter3 exercise 7.py
@@ -2,8 +2,7 @@ Location=["USA","Oman","Mexico","UAE","Kerala"]
 print(Location)
 print(sorted(Location))
 print(sorted(Location,reverse=True))
-Location.reverse()
-print(Location)
+print(reversed(Location))
 print(sorted(Location))
 print(sorted(Location,reverse=True))
 
