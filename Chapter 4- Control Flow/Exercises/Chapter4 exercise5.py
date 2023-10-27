f8c0ff1fbb3e8fd8fec1a197d4c05c1f121a1989
Angel-Mathew <144330644+Angel-Mathew@users.noreply.github.com>
2023-10-27 08:16:52 +0400

    Chapter 4 exercises

---
+++ Chapter 4- Control Flow/Exercises/Chapter4 exercise5.py
@@ -0,0 +1,32 @@
+#Make a list of your favorite fruits, and then write a series of independent if statements that check for certain fruits in your list.
+#•Make a list of your three favorite fruits and call it favorite_fruits.
+#•Write five if statements. Each should check whether a certain kind of fruit is in your list. If the fruit is in your list, the if block 
+#should print a statement,such as You really like bananas!
+# practice
+#favorite_fruits=["Strawberry","Mango","Rambutan"]
+#print(favorite_fruits)
+#favorite_fruits=input('enter the favorite_fruits:')
+#if favorite_fruits=="strawberry":
+    #print("strawberry is from the land of favorite_fruits")
+#if favorite_fruits=="Mango":
+    #print("Mango is from the land of favorite_fruits")
+#if  favorite_fruits=="Rambutan":
+    #print("Mango is from the land of favorite_fruits")
+#else:
+    #print("fruit is not available from the land of favorite_fruits")
+# Answer of the qoution
+favorite_fruits=["Strawberrys","Mangos","Rambutan"]
+print(favorite_fruits)
+favorite_fruits=input('enter the favorite_fruits:')
+if favorite_fruits=="strawberry":
+    print("You really like Strawberry!")
+if favorite_fruits=="Mango":
+    print("You really like Mango!")
+if favorite_fruits=="Rambutan":
+    print("You really like Rambutan!")
+if favorite_fruits=="Banana":
+    print("You really like Banana!")
+if favorite_fruits=="Apple":
+    print("You really like Apple!")
+else:
+     print("You really don't like Fruit")
--- Chapter 4- Control Flow/Exercises/practice else and if statments.py
+++ Chapter 4- Control Flow/Exercises/practice else and if statments.py
@@ -1,3 +1,4 @@
+#practice
 #Salary=float(input("enter salary:"))
 #advance=0
 #if Salary>60000:
@@ -5,10 +6,21 @@
 #else:
     #advance=0
 #print("your advance:"+str(advance))    
-Temperature=float(input("enter the temperature:"))
-About_weather1=("Nice weather,we are having")
-if Temperature<40:
-    About_weather=("Nice weather,we are having")
-else:
-    About_weather=("A little cold, is not it?")    
-print("your About_weather:"+str(About_weather))    
+#Answer of the question showed in the slide in class
+#Temperature=float(input("enter the temperature:"))
+#About_weather1=("Nice weather,we are having")
+#if Temperature<40:
+    #About_weather=("Nice weather,we are having")
+#else:
+    #About_weather=("A little cold, is not it?")    
+#print("your About_weather:"+str(About_weather))
+#nested desicion structure
+earning=int(input("enter your earning per year:"))
+work_experience=float(input("enter your work experience:"))
+if earning>30000:
+ if work_experience>=2:
+        print("you are elegibile for loan")
+ else:
+        print("Sorry your work experience is less than 2 year")
+else: 
+        print("your earning is less than 30000")
