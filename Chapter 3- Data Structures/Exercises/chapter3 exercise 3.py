188fce07685b2510bb707407d017502425bec3f0
Angel-Mathew <144330644+Angel-Mathew@users.noreply.github.com>
2023-10-30 10:23:55 +0400

    Update Chapter 3 exercises

--- Chapter 3- Data Structures/Exercises/chapter3 exercise 3.py
+++ Chapter 3- Data Structures/Exercises/chapter3 exercise 3.py
@@ -1,3 +1,6 @@
+# Exercise 3: Your Own List :ballot_box_with_check:
+#Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list that stores several examples. Use your list
+#to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
 tranportation=["I would like to ride in Impala car","I would like to ride in Toyota car","I would like to travel in double truck bus"]
 print(tranportation)
 tranportation=["I would like to ride in Impala car","I would like to ride in Toyota car","I would like to travel in double truck bus"]
--- Chapter 3- Data Structures/Exercises/chapter3 exercise 4.py
+++ Chapter 3- Data Structures/Exercises/chapter3 exercise 4.py
@@ -1,3 +1,5 @@
+#If you could invite anyone, living or deceased, to dinner, who would you invite? Make a list that includes at least three people you’d
+#like to invite to dinner. Then use your list to print a message to each person, invitingthem to dinner.
 Names=["Ann","Paul","Annette","Mathew"]
 print(Names[0]+"_Inviting all to a dinner on 21 October 2023, at 8:00pm,at Olive Rose Theodore's house")
 print(Names[1]+"_Inviting all to a dinner on 21 October 2023, at 8:00pm,at Olive Rose Theodore's house")
