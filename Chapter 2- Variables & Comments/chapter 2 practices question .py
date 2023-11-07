8e1e18e51f3e448e09faa376627537e51225b41d
Angel-Mathew <144330644+Angel-Mathew@users.noreply.github.com>
2023-11-07 18:41:57 +0400

    chapter 2 practice questions

---
+++ Chapter 2- Variables & Comments/chapter 2 practices question .py
@@ -0,0 +1,22 @@
+# q1 Write a python code that uses three variables x, y, and z with respective values 10,20 and 30. Add all the
+#variables and then store the result in variable a. Print the value of a
+x=10
+y=20
+z=30
+a=x+y+z
+print(a)
+# q2 Write a python program that takes courses marks as input and then calculates average of all the
+#courses. After calculating the average, calculate the percentage of a student using total marks. Assume 
+#the total of all the courses marks is 500 or take the total marks from the user as input. 
+Ip=int(input("enter the marks of Ip:"))
+Computer_Science=int(input("enter the marks of Computer_Science:"))
+Eng=int(input("enter the marks of Eng:"))
+sum=Ip+Computer_Science+Eng
+average=sum/3
+total=int(input("enter the total:"))
+percentage=sum/total*100
+print(percentage)
+#q3 Write a python program that takes an input 5 from user and then type cast those values to string, int
+# and float in the separate variables. Print all the variables
+
+
