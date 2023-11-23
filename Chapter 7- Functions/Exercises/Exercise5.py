0ce4b13e022885844faa6f4103e37d313bc3fd1c
Angel-Mathew <144330644+Angel-Mathew@users.noreply.github.com>
2023-11-23 13:13:20 +0400

    Create Exercise5.py

---
+++ Chapter 7- Functions/Exercises/Exercise5.py
@@ -0,0 +1,9 @@
+# Exercise 5: Cities
+#Write a function called describe_city() that accepts the name of a city and its country. The function should print a simple sentence, 
+#such as Reykjavik is in Iceland. Give the parameter for the country a default value.
+#Call your function for three different cities, at least one of which is not in the default country.
+def describe_city(city,country="United Arab Emirates"):
+    print(city.title()+" is in "+country.title())
+describe_city("Dubai")
+describe_city("Sharjah")
+describe_city("Kerala","India")
